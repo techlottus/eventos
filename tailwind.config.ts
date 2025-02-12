f339ebf3530783bf6e1199ba900450bd92dc7ebf
Maria-cris <maria.delacruz@lottuseducation.com>
2025-02-12 10:35:59 -0600

    Initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,9 @@
-import type { Config } from "tailwindcss";
+/** @type {import('tailwindcss').Config} */
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -15,4 +14,4 @@ export default {
     },
   },
   plugins: [],
-} satisfies Config;
+} 
