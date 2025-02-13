2bff96591b7dbcdd51c2da8ff91eade31598ddb1
Maria-cris <maria.delacruz@lottuseducation.com>
2025-02-13 13:08:23 -0600

    added next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
+const { env } = require('process');
+const strapiUrl = env.NEXT_PUBLIC_STRAPI_URL
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    unoptimized: true,
+    domains: ['drive.google.com','shutterstock.com',`${strapiUrl}/uploads/`]
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"], // Mantén el loader de SVG en Webpack
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
