41388819ddb8568b216fa39eace21da1bfd3c096
Maria-cris <maria.delacruz@lottuseducation.com>
2025-02-13 13:07:49 -0600

    added font links

---
+++ fontlinks.ts
@@ -0,0 +1,7 @@
+const links = [
+  "https://fonts.googleapis.com/css2?&family=Poppins:wght@400;600;700&display=swap",
+  "https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@400;600;700&display=swap",
+  "https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap",
+  "https://fonts.googleapis.com/icon?family=Material+Icons"
+] 
+ export default links;
